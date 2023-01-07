3d7ded061d972f37657f5eb562365d75eaffa728
FebriTahta <febrirtah@gmail.com>
2023-01-07 15:29:26 +0700

    migration db jawaban

---
+++ database/migrations/2023_01_07_082308_create_jawabanmultis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jawabanmultis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('siswa_id')->nullable();
+            $table->unsignedBigInteger('ujian_id')->nullable();
+            $table->unsignedBigInteger('soalmulti_id')->nullable();
+            $table->unsignedBigInteger('optionmulti_id')->nullable();
+            $table->string('jawabanku')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jawabanmultis');
+    }
+};
