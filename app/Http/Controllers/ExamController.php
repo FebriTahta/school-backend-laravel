234b133592d9990a08d72864cf29566b5cc135f1
FebriTahta <febrirtah@gmail.com>
2023-03-04 22:25:40 +0700

    cek loading ujian uraian

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -733,9 +733,13 @@ class ExamController extends Controller
         $kelas_id = Crypt::decrypt($kelas_id);
         $kelas = Kelas::findOrFail($kelas_id);
         $pilihan_ganda_aktif = $kelas->exam->where('exam_status','aktif');
-        $uraian_aktif = $kelas->examurai->where('examurai_status','aktif')->with('jawabanexamurai');
+        $uraian_aktif = $kelas->examurai->where('examurai_status','aktif');
         $siswa = auth()->user()->siswa;
-        return $uraian_aktif;
+        $tes = Exam::whereHas('kelas', function($query) use ($kelas){
+            $query->where('kelas_id', $kelas->id);
+        })->get();
+
+        return $tes;
         return view('fe_page.daftar_pilihan_ganda',compact('kelas','siswa','pilihan_ganda_aktif','uraian_aktif'));
     }
 
