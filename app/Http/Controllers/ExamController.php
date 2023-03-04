4bd9591f0e9fc02fb4ffdbb3911e1f4147a92c07
FebriTahta <febrirtah@gmail.com>
2023-03-04 23:13:57 +0700

    cek loading ujian uraian

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -746,7 +746,7 @@ class ExamController extends Controller
 
         // return $examurai_id;
 
-        $total = Jawabanexamurai::where('kelas_id',$kelas->id)->whereIn('examurai_id',[13])->select('siswa_id')->distinct()->toArray()->get();
+        $total = Jawabanexamurai::where('kelas_id',$kelas->id)->whereIn('examurai_id',[13])->select('siswa_id')->distinct()->get()->toArray();
         return $total->count();
 
         
