02ecc074d363b2718dfe39da55dd649c4012da64
FebriTahta <febrirtah@gmail.com>
2023-03-04 23:40:30 +0700

    fixing export jawaban uraian

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -746,7 +746,7 @@ class ExamController extends Controller
 
         // return $examurai_id;
 
-        $total = Jawabanexamurai::where('kelas_id',$kelas->id)->whereIn('examurai_id',[13,15])->select('siswa_id')->distinct()->get();
+        $total = Jawabanexamurai::where('kelas_id',$kelas->id)->whereIn('examurai_id',[13,15])->count();
         return $total;
 
         
